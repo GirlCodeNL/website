be22e31d73ba0c468661aa328d9954fbb33c3b6f
Katja Hollaar <afkatja@gmail.com>
2016-08-12 15:21:30 +0200

    About - added page and route

--- client/routes.js
+++ client/routes.js
@@ -1,14 +1,38 @@
 FlowRouter.route('/', {
   name: 'welcome',
+  triggersEnter: [function () {
+    Meteor.defer(function () {
+      $('main').addClass('light-theme');
+    });
+  }],
   action: function(){
-    BlazeLayout.render('content', { tutorial: 'welcome' });
+    BlazeLayout.render('content', { content: 'welcome' });
     window.document.title = 'Welcome to GirlCode tutorials!';
   }
-})
+});
+
+FlowRouter.route('/:pageName', {
+  name: 'pages',
+  triggersEnter: [function () {
+    Meteor.defer(function () {
+      $('main').addClass('light-theme');
+    });
+  }],
+  action: function(params){
+    BlazeLayout.render('content', { content: params.pageName });
+    window.document.title = 'GirlCode - ' + params.pageName;
+  }
+});
+
 FlowRouter.route('/tutorials/:pageName', {
     name: 'tutorials',
+    triggersEnter: [function () {
+      Meteor.defer(function () {
+        $('main').addClass('dark-theme');
+      });
+    }],
     action: function(params) {
-      BlazeLayout.render('content', { tutorial: params.pageName });
+      BlazeLayout.render('content', { content: params.pageName });
       window.document.title = params.pageName;
     }
 });
