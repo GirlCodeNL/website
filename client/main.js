742c5c829e97d5b275c146e5c1a496797c431151
Katja Hollaar <afkatja@gmail.com>
2016-08-19 16:47:34 +0200

    Navigation - hover on nav item for subnav

--- client/main.js
+++ client/main.js
@@ -6,10 +6,35 @@ if(Meteor.isClient) {
       return active && 'active';
     }
   });
+
+  let navIsOpen = false;
+  Template.body.events({
+    'mouseover': function(e){
+      if(navIsOpen) {
+        onClickBody(e);
+      }
+    }
+  });
+
+  function onClickBody(ev) {
+    let isOnNav = $(ev.target).parents('.subnav').length > 0 || ev.target.classList.contains('has-subnav');
+    if (!isOnNav) {
+      toggleNav();
+    }
+  }
+
+  function toggleNav(){
+    navIsOpen = !navIsOpen;
+    document.querySelector('.subnav').classList[navIsOpen ? 'add' : 'remove']('expanded');
+  }
+
   Template.nav.events({
     'click li a': function (e) {
       const url = e.target.getAttribute('href');
       FlowRouter.go(url);
+    },
+    'mouseover .has-subnav': function(e) {
+      toggleNav();
     }
   });
 
